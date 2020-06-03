deb99129c22342297a97ff7ebc1bb588c7e49be4
pythonxyx <xyxgary@126.com>
2020-06-03 17:58:08 +0800

    完善基本查询功能

--- 临时测试.py
+++ 临时测试.py
@@ -2,17 +2,50 @@ import os
 import shutil
 
 
-#测试遍历文件夹
-def FindAll(xpath='.\\程序文件夹'):
-    for x in os.listdir(xpath):
-        next_file = os.path.join(xpath,x)
-        if os.path.isfile(next_file):
-            n=os.path.relpath(next_file)
-            tmplist = n.split('\\')
-            print(tmplist)
-            newname = tmplist[2]
+# #测试遍历文件夹
+# def FindAll(xpath='.\\程序文件夹'):
+#     for x in os.listdir(xpath):
+#         next_file = os.path.join(xpath,x)
+#         if os.path.isfile(next_file):
+#             n=os.path.relpath(next_file)
+#             tmplist = n.split('\\')
+#             print(tmplist)
+#             newname = tmplist[2]
+#
+#         else:
+#             FindAll(next_file)
+#
+# FindAll()
+# count =0
+# countfiles = 0
+# x = os.walk('.\\查找目标源文件夹')
+# for root,dirs,files in x:
+#     for each in dirs:
+#         count += 1
+#     for each in files:
+#         countfiles +=1
+# print('文件夹的数量为：{}'.format(count))
+# print('文件的数量为：{}'.format(countfiles))
+#
 
-        else:
-            FindAll(next_file)
+# print(x)
+# print(y)
+# print(z)
 
-FindAll()
+#测试显示查找到的文件数量,测试成功
+# count = 0
+# count1 = 0
+# def FindAll(path):
+#     global count
+#     global count1
+#     for x in os.listdir(path):
+#         next_file = os.path.join(path,x)
+#         if os.path.isfile(next_file):
+#             count += 1
+#         else:
+#             count1 += 1
+#             FindAll(next_file)
+#     return count,count1
+#
+# x,y= FindAll('.\\查找目标源文件夹')
+# print(x,y)
--- 在文件夹中查找指定的文件.py
+++ 在文件夹中查找指定的文件.py
@@ -1,27 +1,91 @@
 import os
 import shutil
+import re
 
-#测试遍历文件夹
-def FindAndCopyMove(xplace='.\\程序文件夹'):
-    for x in os.listdir(xplace):
-        next_file = os.path.join(xplace,x)
+#关键字查找文件，不重新命名
+def FindAndCopyMoveOne(path,str1):
+    global filecount
+    for x in os.listdir(path):
+        next_file = os.path.join(path,x)
         if os.path.isfile(next_file):
-            if 'test' in x:
-                if not os.path.exists('.\\目标目录'):
-                    os.mkdir('.\\目标目录')
-                newfile = os.path.join('.\\目标目录',x)
-                filenamelist = x.split('.')
-                tmplist = os.path.relpath(next_file).split('\\')
-                newfilename = tmplist[2]+'.'+filenamelist[1]
-                newname = os.path.join('.\\目标目录',newfilename)
-                shutil.copy(next_file,newfile)
-                try:
-                    os.rename(newfile,newname)
-                except:
-                    print('【{}】文件夹中找到多个符合的文件，已经存在，不再重复复制'.format(tmplist[2]))
-                print(x,'已经被复制且改名！')
+            for keyword in str1:
+                if keyword in x:
+                    filecount += 1
+                    filenamelist = x.split('.')
+                    newfilename = filenamelist[0]+str(filecount)+'.'+filenamelist[1]
+                    os.renames(next_file,os.path.join('.\\查找结果文件夹',newfilename))
         else:
-            FindAndCopyMove(next_file)
+            FindAndCopyMoveOne(next_file,str1)
 
-FindAndCopyMove()
+def MainProgram():
+    while not os.path.exists('.\\查找目标源文件夹'):
+        os.mkdir('.\\查找目标源文件夹')
+        sourcedir = '.\\查找目标源文件夹'
+    while not os.path.exists('.\\查找结果文件夹'):
+        os.mkdir('.\\查找结果文件夹')
+        resultdir = '.\\查找结果文件夹'
+    while True:
+        i=os.system('cls')
+        print('*'*10+'欢迎使用本程序'+'*'*10)
+        print('\n')
+        print('【说明】：本程序可以通过文件名字的关键词用来查找1个或多个文件夹中你需要的文件！\n')
+        print('''【使用方法】：
+    1.将需要的查找的1个或多个文件夹复制到本程序所在位置的【查找目标源文件夹】（首次运行本程序时会自动创建该文件夹），
+    2.选择查找要实现的功能
+    3.输入查找的文件名称中的关键词
+    4.在【查找结果文件夹】中获得相应的结果
+    5.可以在【查找结果文件夹】中点击《查找结果.txt》文件来查看查询的情况
+        \n''')
+        print('【提示】：重复使用时，请注意清空【查找结果文件夹】！！')
+        print('-'*27+'\n')
+        print('''请选择相应的功能：
+    1.仅查找文件，不重新命名（遇重名时自动编号）
+    2.查找文件，并用文件所在的文件夹名字命名''')
+        print('*'*27)
+        chosed = input('请选择相应功能（直接回车键退出程序）：')
+        if chosed == '1':
+            while True:
+                i = os.system('cls')
+                print('*'*10+'目前正在使用【仅查找文件，不重新命名（遇重名时自动编号）】功能'+'*'*10)
+                input('\n请将需要查找的文件夹或文件拷贝到【查找目标源文件夹】，拷贝完成后回车键继续…')
+                DirNums,FileNums = CountFileAndDir()
+                if DirNums == 0 and FileNums == 0:
+                    print('没有在【查找目标源文件夹】中找到任何文件夹或文件，请确认已经将需要查找的'
+                          '文件拷入到该文件夹')
+                else:
+                    print('已经在【查找目标源文件夹】中找到{}个文件夹，{}个文件！'.format(DirNums,FileNums))
+                chosed = input('\n是否开始查找？（0-返回主界面，直接回车键开始查找！）')
+                if chosed == '0':
+                    break
+                else:
+                    keyword = input('请输入文件名的关键词(多个关键词，请用逗号<不区分中英文>隔开):')
+                    keywordlist = re.split('[,，]',keyword)
+                    print('正在查找并拷贝文件，请稍后…')
+                    FindAndCopyMoveOne('.\\查找目标源文件夹', keywordlist)
+                    print('查找结束')
 
+
+
+
+        elif chosed == '2':
+            pass
+        else:
+            break
+
+
+#  定义一个函数，来统计【查找目标源文件夹】中文件夹和文件的数量
+def CountFileAndDir(path='.\\查找目标源文件夹'):
+    DirNums = 0
+    FileNums = 0
+    for root,dirs,files in os.walk(path):
+        for each in dirs:
+            DirNums += 1
+        for each in files:
+            FileNums += 1
+    return DirNums,FileNums
+
+filecount = 0  # 定义一个全局变量，来统计找到的文件数量，并用来区别重名文件的命名
+MainProgram()
+
+# x,y= CountFileAndDir()
+# print(x,y)
